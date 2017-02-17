c6352e82b4366fac7db6c1bf25120683007fa564
andcliu <andcliu@gmail.com>
2017-02-16 20:33:04 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="font_awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Quattrocento" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+<body>
+
+ <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+<!--         <a href="#!" class="brand-logo">Logo</a> -->
+        <ul class="left hide-on-med-and-down">
+            <li><a class="page-scroll" href="#header"><i class="material-icons">home</i></a></li>
+			<li><i class="star material-icons">panorama_fish_eye</i></li>
+          <li><a class="page-scroll" href="#about">About</a></li>
+			<li><i class="star material-icons">panorama_fish_eye</i></li>
+          <li><a class="page-scroll" href="#menu">Chef</a></li>
+			<li><i class="star material-icons">panorama_fish_eye</i></li>
+          <li><a class="page-scroll" href="#info">Menu</a></li>
+
+			<li><i class="star material-icons">panorama_fish_eye</i></li>
+          <li><a class="page-scroll" href="#gallery">Gallery</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+        
+
+  <div id="header">
+  	<div id="h1" class="animated rotateInDownLeft">
+	  	<img src="images/h1.png">
+	</div>
+	<div id="h6" class="animated rotateInDownLeft">
+	<img src="images/h6.png">
+	</div>
+  	<div id="header_text" class="animated fadeIn">
+	  	<h1 class="title"> 
+	  		<img src="images/logo.png"> 
+	  		<br>
+	  		Yoshio Sushi <br>
+	  		<img style="width:35%;" src="images/border.png">
+	  	</h1>
+	</div>
+	<div id="h5" class="animated rotateInDownLeft">
+		<img src="images/h5.png">
+	</div>
+	<div id="h2" class="animated rotateInDownLeft">
+	  	<img src="images/h2.png">
+	</div>
+	<div id="h4" class="animated rotateInDownLeft">
+  		<img src="images/h4.png">
+  	</div>
+
+  </div>
+
+
+  <div id="about" class="parallax-container">
+  	<div class="row">
+  		<div class="col m4 offset-m7" id="about_text">
+  			<h3 class="title center-align"> Traditionality & Exclusivity  </h3>
+  			<p>
+	  			Yoshio Sushi is a Japanese restaurant with traditional ambiance crafting exclusive signature rolls, fresh sashimi, nabemono (hot pots) & shabu-shabu, bento boxes and more in our izakaya “sushi bar,” dining tables, and VIP zashiki & horigotatsu (taking your shoes off & sitting on tatami and pillows) style room.
+	  		</p>
+  		</div>
+  	</div>
+    <div class="parallax"><img src="images/yoshio.jpg"></div>
+
+  </div>
+  <div id="chef">
+  	<div class="row">
+  		<div class="col m4" id="chef_text">
+  			<h3 class="title center-align"> The Itamae </h3>
+  			<p>
+	  			At the age of 18, Johnny Chang co-opened a Japanese restaurant in Taiwan. Now as owner and senior itamae-san (sushi chef) of Yoshio Sushi, formerly known as Taki Sushi, he brings traditional Japanese cuisine and izakaya sushi bar experience to his patrons. Johnny transformed Taki Sushi, which was originally co-owned by one of the founding fathers of the California Roll, to Yoshio Sushi in early 2016. Named after Johnny's father, Yoshio, he pays homage and brings to life his father's experiences and his true passions of appreciating Japanese cuisine. 
+	  		</p>
+			<div id="wrapper">
+				<ul id="list">
+					<li>
+						<p>
+							Yoshio Sushi's integrity of tradition, authenticity, family, combination of quality and quantity, real ingredients, and with the feel of coming home to eat, is mirrored to Johnny's relationship with his father, keeping it real, home, quality, family. We here at Yoshio don't chase fancy ambiances, but hand select every fresh fish to give you real Japanese home food. Johnny embodies that passion to serve you with that same experience of authentic, traditional, family style, fresh sushi to you, your table, and in our VIP zashiki/horigotatsu seating rooms. If you ever visit Japan, you will experience Yoshio Sushi.
+						</p>
+					</li>
+				</ul>
+	  		</div>
+			<a id="button" class="btn toggle-text right">
+                <span>Read More</span><span class="hidden">Read Less</span>...
+            </a>
+  		</div>
+  	</div>
+  </div>
+
+  <div id="menu" class="parallax-container">
+  			<div class="col m12 center-align">
+  				<h3 class="title"> Main Menu <br>
+		<img style="width:15%; margin-top:-1%;" src="images/border.png"></h3>
+  			</div>
+	  <div class="carousel">
+	    <a class="carousel-item" href="#one!"><img src="images/dummy.jpg"></a>
+	    <a class="carousel-item" href="#two!"><img src="images/dummy.jpg"></a>
+	    <a class="carousel-item" href="#three!"><img src="images/dummy.jpg"></a>
+	    <a class="carousel-item" href="#four!"><img src="images/dummy.jpg"></a>
+	    <a class="carousel-item" href="#five!"><img src="images/dummy.jpg"></a>
+	  </div>
+      <div class="parallax"><img src="images/men2.jpg"></div>
+  </div>
+  
+
+  <div id="gallery">
+	<div class="col m12 center-align">
+		<h3 class="title"> Picture Gallery <br>
+		<img style="width:15%; margin-top:-1%;" src="images/border.png"></h3>
+	</div>
+<div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:960px;height:480px;overflow:hidden;visibility:hidden;background-color:#24262e; border:.1px solid #000;">
+        <!-- Loading Screen -->
+        <div data-u="loading" style="position:absolute;top:0px;left:0px;background-color:rgba(0,0,0,0.7);">
+            <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+            <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+        </div>
+        <div data-u="slides" style="cursor:default;position:relative;top:0px;left:240px;width:720px;height:480px;overflow:hidden;">
+            <a data-u="any" href="http://www.jssor.com" style="display:none">Image Gallery with Vertical Thumbnail</a>
+            <div>
+                <img data-u="image" src="img/s1.jpeg" />
+                <img data-u="thumb" src="img/s1.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s2.jpeg" />
+                <img data-u="thumb" src="img/s2.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s3.jpeg" />
+                <img data-u="thumb" src="img/s3.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s4.jpeg" />
+                <img data-u="thumb" src="img/s4.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s1.jpeg" />
+                <img data-u="thumb" src="img/s1.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s2.jpeg" />
+                <img data-u="thumb" src="img/s2.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s3.jpeg" />
+                <img data-u="thumb" src="img/s3.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s4.jpeg" />
+                <img data-u="thumb" src="img/s4.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s1.jpeg" />
+                <img data-u="thumb" src="img/s1.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s2.jpeg" />
+                <img data-u="thumb" src="img/s2.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s3.jpeg" />
+                <img data-u="thumb" src="img/s3.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s4.jpeg" />
+                <img data-u="thumb" src="img/s4.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s3.jpeg" />
+                <img data-u="thumb" src="img/s3.jpeg" />
+            </div>
+            <div>
+                <img data-u="image" src="img/s4.jpeg" />
+                <img data-u="thumb" src="img/s4.jpeg" />
+            </div>
+        </div>
+        <!-- Thumbnail Navigator -->
+        <div data-u="thumbnavigator" class="jssort01-99-66" style="position:absolute;left:0px;top:0px;width:240px;height:480px;background-color: #011;" data-autocenter="2">
+            <!-- Thumbnail Item Skin Begin -->
+            <div data-u="slides" style="cursor: default;">
+                <div data-u="prototype" class="p">
+                    <div class="w">
+                        <div data-u="thumbnailtemplate" class="t"></div>
+                    </div>
+                    <div class="c"></div>
+                </div>
+            </div>
+            <!-- Thumbnail Item Skin End -->
+        </div>
+        <!-- Arrow Navigator -->
+        <span data-u="arrowleft" class="jssora05l" style="top:0px;left:248px;width:40px;height:40px;color:#fff;" data-autocenter="2"></span>
+        <span data-u="arrowright" class="jssora05r" style="top:0px;right:8px;width:40px;height:40px;color:#fff;" data-autocenter="2"></span>
+    </div>
+  </div>
+
+        <footer class="page-footer" id="foot">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text title">Business Information</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="title white-text">Stay Connected</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Google +</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright" id="botfoot">
+            <div class="container title center-align">
+            © 2017 Copyright. Yoshio Sushi. All Rights Reserved.
+            <a class="grey-text text-lighten-4 right" href="#!"></a>
+            </div>
+          </div>
+        </footer>
+</body>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="javascript/jssor.slider-22.2.6.mini.js" type="text/javascript"></script>
+	<script type="text/javascript" src="javascript/materialize.min.js"></script>
+	<script type="text/javascript">
+	    $(document).ready(function(){
+			$('.carousel').carousel();
+		      $('.parallax').parallax();
+
+            $('.hidden').removeClass('hidden').hide();
+            $('.toggle-text').click(function() {
+                $(this).find('span').each(function() { $(this).toggle(); });
+            });
+
+	    });
+
+		$(function() {
+			var b = $("#button");
+			var w = $("#wrapper");
+			var l = $("#list");
+
+			// w.height(l.outerHeight(true)); REMOVE THIS 
+
+			b.click(function() {
+
+			if (w.hasClass('open')) {
+			  w.removeClass('open');
+			  w.height(0);
+			} else {
+			  w.addClass('open');
+			  w.height(l.outerHeight(true));
+			}
+
+		});
+	});
+
+        jQuery(document).ready(function ($) {
+
+            var jssor_1_SlideshowTransitions = [
+              {$Duration:1200,$Zoom:1,$Easing:{$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad},$Opacity:2},
+              {$Duration:1000,$Zoom:11,$SlideOut:true,$Easing:{$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:1200,$Zoom:1,$Rotate:1,$During:{$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
+              {$Duration:1000,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
+              {$Duration:1200,x:0.5,$Cols:2,$Zoom:1,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:1200,x:4,$Cols:2,$Zoom:11,$SlideOut:true,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:1200,x:0.6,$Zoom:1,$Rotate:1,$During:{$Left:[0.2,0.8],$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Left:$Jease$.$Swing,$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
+              {$Duration:1000,x:-4,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
+              {$Duration:1200,x:-0.6,$Zoom:1,$Rotate:1,$During:{$Left:[0.2,0.8],$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Left:$Jease$.$Swing,$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
+              {$Duration:1000,x:4,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
+              {$Duration:1200,x:0.5,y:0.3,$Cols:2,$Zoom:1,$Rotate:1,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.7}},
+              {$Duration:1000,x:0.5,y:0.3,$Cols:2,$Zoom:1,$Rotate:1,$SlideOut:true,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InExpo,$Top:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.7}},
+              {$Duration:1200,x:-4,y:2,$Rows:2,$Zoom:11,$Rotate:1,$Assembly:2049,$ChessMode:{$Row:28},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.7}},
+              {$Duration:1200,x:1,y:2,$Cols:2,$Zoom:11,$Rotate:1,$Assembly:2049,$ChessMode:{$Column:19},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.8}}
+            ];
+
+            var jssor_1_options = {
+              $AutoPlay: true,
+              $SlideshowOptions: {
+                $Class: $JssorSlideshowRunner$,
+                $Transitions: jssor_1_SlideshowTransitions,
+                $TransitionsOrder: 1
+              },
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $ThumbnailNavigatorOptions: {
+                $Class: $JssorThumbnailNavigator$,
+                $Rows: 2,
+                $Cols: 6,
+                $SpacingX: 14,
+                $SpacingY: 12,
+                $Orientation: 2,
+                $Align: 156
+              }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            /*responsive code begin*/
+            /*you can remove responsive code if you don't want the slider scales while window resizing*/
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 960);
+                    refSize = Math.max(refSize, 300);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+            ScaleSlider();
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            /*responsive code end*/
+        });
+
+	</script>
+</html>
